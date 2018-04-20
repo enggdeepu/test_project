3f522628a76c8cf9b9b54c099603c3a6fabc2385
Deepak Singh <engg.deepu@gmail.com>
2018-04-20 17:29:56 +0530

    d file added

---
+++ d.php
@@ -0,0 +1,2 @@
+msisdn	imei1	destination_ip	destination_port	source_ip	imei2	imsi	downlink_vol	uplink_vol	session_start_time	
+session_end_time	pre_post	home_roaming_circle	       roaming_network_indicator
